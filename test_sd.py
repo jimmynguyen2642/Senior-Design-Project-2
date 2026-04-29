7b5ab69e30d74f319996ef1ead0d42b214bce33a
Jimmy Nguyen <jimmy.nguyen10@okstate.edu>
2026-04-28 22:26:06 -0500

    sd card test

--- test_sd.py
+++ test_sd.py
@@ -1,31 +1,19 @@
-import board
-import busio
-import digitalio
-import adafruit_sdcard
-import storage
+import spidev
 
-# SPI setup
-spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+# Test 1: SPI is responding
+spi = spidev.SpiDev()
+spi.open(0, 0)
+spi.max_speed_hz = 1000000
+spi.mode = 0
+resp = spi.xfer2([0xFF] * 10)
+print("SPI response:", [hex(x) for x in resp])
+spi.close()
 
-# CS pin (GPIO8 / Pin 24)
-cs = digitalio.DigitalInOut(board.D8)
-
-# Initialize SD card
-sdcard = adafruit_sdcard.SDCard(spi, cs)
-
-# Mount filesystem
-vfs = storage.VfsFat(sdcard)
-storage.mount(vfs, "/sd")
-
-print("SD card mounted!")
-
-# Write file
-with open("/sd/test.txt", "w") as f:
-    f.write("Hello from Raspberry Pi!\n")
-
-print("Write successful!")
-
-# Read file back
-with open("/sd/test.txt", "r") as f:
-    print("File contents:")
-    print(f.read())
+# Test 2: SD card seen as block device
+import os
+if os.path.exists("/dev/sda"):
+    print("SD card found at /dev/sda")
+elif os.path.exists("/dev/sda1"):
+    print("SD card found at /dev/sda1")
+else:
+    print("No SD block device found - wiring or overlay issue")
